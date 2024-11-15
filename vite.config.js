a88ce356634bc7e378ddcb442e413ecf889eef70
Boasfar27 <muhammadfarhan2747@gmail.com>
2024-11-15 22:10:01 +0700

    done deploy:domainesia

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/PORTOFOLIO-WEB/",
-    plugins: [react()],
+  build: {
+    outDir: 'build', // Ganti 'build' dengan nama folder output yang Anda inginkan
+  },
+  plugins: [react()],
 })
