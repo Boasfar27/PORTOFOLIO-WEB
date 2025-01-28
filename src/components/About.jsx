65e163f46d7b2107f3e97114b8501fb067e892f3
Boasfar27 <muhammadfarhan2747@gmail.com>
2025-01-28 17:27:44 +0700

    components : about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -83,7 +83,7 @@ function About() {
             data-aos="fade-up"
             data-aos-delay="200"
           >
-            Saya adalah mahasiswa Program Studi Sistem Informasi di Universitas Negeri Surabaya, saat ini sedang menempuh&nbsp;
+            Saya mahasiswa Program Studi Sistem Informasi di Universitas Negeri Surabaya, saat ini sedang menempuh&nbsp;
             <span className="text-orange-500">semester 4</span>
             &nbsp;Menjadi seorang programmer sudah menjadi cita-cita saya sejak 3 tahun lalu saat masih duduk di bangku SMA. Saya sangat tertarik untuk mengembangkan kemampuan di bidang teknologi informasi, khususnya pengembangan website dengan fokus sebagai Front-End Developer. Saya juga aktif di organisasi HIPMI PT UNESA sebagai Ketua Bidang Industri Kreatif dan Sosial Media, dan memiliki semangat untuk terus belajar agar dapat mengikuti perkembangan teknologi yang terus berkembang di masa kini dan masa yang akan datang.
           </motion.p>
